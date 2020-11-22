51e9e92172d307c42d86de4e2fab5028d109bb97
QcFe <io@qcfe.me>
2020-11-22 21:33:19 +0100

    feat: enable non-interactive mode

--- startup.sh
+++ startup.sh
@@ -8,8 +8,8 @@ trap "cleanup" INT
 
 rm -rfv /tmp/.X*-lock /tmp/.X11-unix
 
-vncserver $DISPLAY -SecurityTypes None
+vncserver $DISPLAY -SecurityTypes None -noxstartup
 
-read -n 1 -r -s -p $'Press any key to exit\n'
+fluxbox
 
 cleanup
